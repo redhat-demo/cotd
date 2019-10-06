058fadbf25ee010bb7eb62cea7d6de8bfee95332
root <root@bastion.example.internal>
2019-10-06 15:22:09 +0800

    add files

--- item.php
+++ item.php
@@ -95,15 +95,15 @@ $help = $help."<p style='color:silver;font-size:80%;font-style:italic'> Enjoy, S
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>COTD</title>
-	<!-- <link rel="stylesheet"  href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css"> -->
-	<link rel="stylesheet"  href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+	<!-- <link rel="stylesheet"  href="css/jquery.mobile-1.3.2.min.css"> -->
+	<link rel="stylesheet"  href="css/jquery.mobile-1.4.5.min.css">
 	<link rel="stylesheet" href="css/jqm-demos.css">
 	<link rel="stylesheet" href="css/swipe-page.css">
 	<link rel="shortcut icon" href="css/favicon.ico">
 	<script src="js/jquery.js"></script>
 	<script src="js/index.js"></script>
-	<!-- <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script> -->
-	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<!-- <script src="js/jquery.mobile-1.3.2.min.js"></script> -->
+	<script src="js/jquery.mobile-1.4.5.min.js"></script>
 	<script src="js/swipe-page.js"></script>
 	<script src="js/rateThis.js"></script>
 
